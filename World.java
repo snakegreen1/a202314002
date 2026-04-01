e014bd6c8038cc93dde6ab055086bccef08c368a
snakegreen1 <6snake0green9@gmail.com>
2026-04-01 10:48:41 +0900

    five

--- World.java
+++ World.java
@@ -3,5 +3,9 @@ public class World {
     public static void run() {
         System.out.println("202314002");
     }
+
+    public static void run2() {
+        System.out.peinrln("hello world");
+    }
     
 }
